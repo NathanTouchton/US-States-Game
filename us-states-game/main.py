9764c95125dfa69b68b323b5379485a2062e740b
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-07-09 17:19:08 -0500

    Add files via upload

--- us-states-game/main.py
+++ us-states-game/main.py
@@ -21,10 +21,7 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="Name a state below. Keep going until you've named them all!").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
